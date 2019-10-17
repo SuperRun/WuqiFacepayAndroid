431f28e902a6d3d921d85cfab02946756bd21479
SuperRun <445679589@qq.com>
2019-10-17 20:24:08 +0800

    成功获取authinfo

--- app/src/main/java/com/wuqi/facepay/util/Config.java
+++ app/src/main/java/com/wuqi/facepay/util/Config.java
@@ -7,9 +7,9 @@ package com.wuqi.facepay.util;
 public class Config {
 
 
-    //    // 为了apiKey,secretKey为您调用百度人脸在线接口的，如注册，比对等。
-//    // 为了的安全，建议放在您的服务端，端把人脸传给服务器，在服务端端
-//    // license为调用sdk的人脸检测功能使用，人脸识别 = 人脸检测（sdk功能）  + 人脸比对（服务端api）
+    // 为了apiKey,secretKey为您调用百度人脸在线接口的，如注册，比对等。
+    // 为了的安全，建议放在您的服务端，端把人脸传给服务器，在服务端端
+    // license为调用sdk的人脸检测功能使用，人脸识别 = 人脸检测（sdk功能）  + 人脸比对（服务端api）
     public static String apiKey = "MjbYmagnmpEfuIlc1D97oSR1";
     public static String secretKey = "d7hlN0rmOZVQPta7ykWQix8UbRD4mURC";
     public static String licenseID = "wuqi-facepay-face-android";
@@ -22,5 +22,13 @@ public class Config {
      * 人脸注册 接口 https://aip.baidubce.com/rest/2.0/face/v3/faceset/user/add
      */
     public static String groupID = "groupID1";
+
+    // 微信公众号
+    public static String appId = "wx8c97a99600b02985";
+    // 商户号
+    public static String mchId = "1554649691";
+    // api密钥
+    public static String api = "hangzhouwuqishuzikej201908145757";
+
 
 }
